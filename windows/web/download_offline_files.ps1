56899b1937591357587de7c59694b59af4c623f5
Butterscotch! <bscotchvanilla@gmail.com>
2024-09-26 04:48:39 -0400

    Server v0.15.0

--- windows/web/download_offline_files.ps1
+++ windows/web/download_offline_files.ps1
@@ -59,7 +59,7 @@ Function Get-File-From-Uri {
 $DestDir = New-Item (& { $Env:WINDOWS_OFFLINE_FILES_DIR ?? "offline-files" }) -ItemType directory -Force
 Write-Output "Output directory set to ""$DestDir"""
 
-$ServerVersion = "v0.0.19"
+$ServerVersion = "v0.0.15"
 $DriverVersion = "v0.0.3"
 
 $SharedDir = New-Item (Join-Path $DestDir "versions") -ItemType directory -Force
